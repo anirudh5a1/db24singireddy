bfa7877ab3b4bdd279ce7c33eb576186f9e72ec2
anirudh5a1 <89654691+anirudh5a1@users.noreply.github.com>
2021-11-30 19:32:47 -0600

    created application

--- routes/melon.js
+++ routes/melon.js
@@ -1,11 +1,19 @@
 var express = require('express'); 
 const melon_controlers= require('../controllers/melon'); 
 var router = express.Router(); 
+
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
  
 /* GET melons */ 
 router.get('/', melon_controlers.melon_view_all_Page ); 
 router.get('/detail', melon_controlers.melon_view_one_Page); 
 router.get('/create', melon_controlers.melon_create_Page); 
-router.get('/update', melon_controlers.melon_update_Page); 
+router.get('/update', secured , melon_controlers.melon_update_Page); 
 router.get('/delete', melon_controlers.melon_delete_Page); 
 module.exports = router; 
